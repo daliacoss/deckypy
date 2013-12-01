457d041bfca4bfa5b748c897a29792a69a6f9964
deckman <cosstropolis@gmail.com>
2013-11-30 21:12:37 -0500

    flushChannel works correctly

--- decky.py
+++ decky.py
@@ -3,9 +3,11 @@ deckyPy - a simple virtual MIDI keyboard
 by Deckman Coss (cosstropolis.com)
 """
 
-import rtmidi, time
+import rtmidi, os, imp
 from optparse import OptionParser
 from getch import *
+
+CWD = os.path.dirname(os.path.realpath(__file__))
 
 #key constants
 ESC = 27
@@ -60,11 +62,16 @@ class Controller:
 		"\t":"flush",
 		"'":"toggleSustain",
 		"\"":"toggleMonophonic",
+		"`":"toggleVerbose",
 		"[":"noteOffNewest",
 		"]":"noteOffOldest",
 		"\\":"flushChannel",
+		"-":"octaveDown",
 		"=":"octaveUp",
-		"-":"octaveDown"
+		"{":"velocityDown",
+		"}":"velocityUp",
+		"_":"channelDown",
+		"+":"channelUp"
 	}
 	_defaultChannel = 0
 	_defaultVelocity = 127
@@ -94,6 +101,7 @@ class Controller:
 		self.sustain = sustain
 		self.monophonic = monophonic
 
+		print "Channel " + str(self.channel+1)
 		print "Sustain " + ("ON" if self.sustain else "OFF")
 		print "Monophony " + ("ON" if self.monophonic else "OFF")
 		print "Octave " + str(self.octave)
@@ -116,7 +124,7 @@ class Controller:
 			self.stopNote(channel, pitch)
 
 		if self.verbose:
-			print ("note on: channel {0}, pitch {1}".format(msg[0],msg[1]))
+			print ("note on: {0} {1}".format(msg[0],msg[1]))
 
 	def stopNote(self, channel, pitch):
 		msg = (NOTE_ON + channel, pitch, 0)
@@ -127,7 +135,7 @@ class Controller:
 			#only look for the channel and pitch
 			self._activeMsgs.remove(msg[:2])
 			if self.verbose:
-				print ("note off: channel {0}, pitch {1}".format(msg[0],msg[1]))
+				print ("note off: {0} {1}".format(msg[0],msg[1]))
 		except ValueError:
 			pass
 
@@ -137,13 +145,22 @@ class Controller:
 		self.mout.close_port()
 		del self.mout
 
-	def _flush(self, channel=None):
-		for i in range(len(self._activeMsgs)):
-			msg = self._activeMsgs[0]
+	def _flush(self, channel=None, i=0):
+		
+		if i<len(self._activeMsgs):
+			msg = self._activeMsgs[i]
 			if channel != None:
-				if msg[0] - NOTE_ON == channel: self.stopNote(msg[0]-NOTE_ON,msg[1])
+				print msg[0] - NOTE_ON, channel
+				if msg[0] - NOTE_ON == channel:
+					self.stopNote(msg[0]-NOTE_ON,msg[1])
+				#keep searching through _activeMsgs for channel
+				else:
+					i += 1
+				self._flush(channel, i)
 			else:
 				self.stopNote(msg[0]-NOTE_ON,msg[1])
+				#no need to increment i if we're flushing all channels
+				self._flush()
 
 	def flush(self):
 		self._flush()
@@ -159,6 +176,10 @@ class Controller:
 		self.monophonic = not self.monophonic
 		print "Monophony " + ("ON" if self.monophonic else "OFF")
 
+	def toggleVerbose(self):
+		self.verbose = not self.verbose
+		print "Verbose " + ("ON" if self.verbose else "OFF")
+
 	def octaveUp(self):
 		#prevent octave from going above 10
 		self.octave = min(self.octave+1, 10)
@@ -169,18 +190,61 @@ class Controller:
 		self.octave = max(self.octave-1, 0)
 		print "Octave " + str(self.octave)
 
-	def noteOffOldest(self):
-		if len(self._activeMsgs):
-			msg = self._activeMsgs[0]
+	def velocityUp(self):
+		self.velocity = min(self.velocity+16, DATA_BYTE)
+		print "Velocity " + str(self.velocity)
+
+	def velocityDown(self):
+		self.velocity = max(self.velocity-16, 0)
+		print "Velocity " + str(self.velocity)
+
+	def _noteOffOne(self, direction, channel=None):
+		"""if direction > 0, search from the front (oldest)"""
+
+		#direction must be negative or positive
+		if not direction: return
+
+		numMsg = len(self._activeMsgs)
+
+		if numMsg:
+			#if no channel specified, grab the last (newest) or first (oldest)
+			if channel == None:
+				msg = self._activeMsgs[-1 if direction < 0 else 0]
+			#else go through messages in specified direction
+			else:
+				a = 0 if direction > 0 else numMsg
+				b = numMsg * (not a) #0 if a is numMsg
+				for i in range(a, b, direction):
+					msg = self._activeMsgs[i]
+					if msg[0]-NOTE_ON == channel: break
+
 			self.stopNote(msg[0]-NOTE_ON,msg[1])
+
+	def noteOffOldestChannel(self):
+		self._noteOffOne(1,self.channel)
+
+	def noteOffNewestChannel(self):
+		self._noteOffOne(-1,self.channel)
+
+	def noteOffOldest(self, channel=None):
+		self._noteOffOne(1)
+		# if len(self._activeMsgs):
+		# 	msg = self._activeMsgs[0]
+		# 	self.stopNote(msg[0]-NOTE_ON,msg[1])
 
 	def noteOffNewest(self):
-		if len(self._activeMsgs):
-			msg = self._activeMsgs[-1]
-			self.stopNote(msg[0]-NOTE_ON,msg[1])
+		self._noteOffOne(-1)
+
+	def channelUp(self):
+		self.channel = min(self.channel+1, CHANNELS-1)
+		print "Using channel " + str(self.channel+1)
+
+	def channelDown(self):
+		self.channel = max(self.channel-1, 0)
+		print "Using channel " + str(self.channel+1)
 
 	def start(self):
-		print ("Controller is ready")
+		print ("CONTROLLER IS READY")
 		c = getch()
 		while ord(c) != ESC:
 			pitch = self.pitchMap.get(c)
@@ -203,12 +267,14 @@ class Helper:
 		parser = OptionParser()
 		parser.add_option("-p", "--portname", dest="portName",
 			default="deckyPy MIDI-Out",
-			help="name to use for virtual MIDI port")
+			help="name to use for virtual MIDI port",
+			metavar="PORT_NAME")
 		parser.add_option("-s", "--showports", dest="showPorts",
 			action="store_true", default=False,
 			help="show available MIDI ports to choose from")
 		parser.add_option("-f", "--file", dest="mapFile",
-			help="use keymappings in MAP_FILE", metavar="MAP_FILE")
+			help="use keymappings in MAP_FILE (will look in ",
+			metavar="MAP_FILE")
 		parser.add_option("-c", "--channel", dest="channel",
 			default=1,
 			help="default midi channel (1-indexed)")
@@ -218,17 +284,55 @@ class Helper:
 		parser.add_option("-v", "--verbose", dest="verbose",
 			action="store_true", default=False,
 			help="print every MIDI message sent")
+		parser.add_option('-u', "--sustain", dest="sustain",
+			action="store_true", default=False,
+			help="start in sustain mode")
+		parser.add_option('-m', "--monophonic", dest="monophonic",
+			action="store_true", default=False,
+			help="start in monophony")
 
 		self.opts, self.args = parser.parse_args()
 
 	def createController(self):
 		opts = self.opts
 		mout = self._getMout(opts.showPorts, opts.portName)
+		return self._controllerFromArgs(opts, mout)
+
+	def loadKeyMap(self, fname, folder="keymaps"):
+		"""attempt to return keymap from file, else return exception type"""
+		try:
+			km = imp.load_source('decky_keymap', os.path.join(CWD,folder,fname))
+			return km.pitchMap, km.funcMap
+		except (IOError, AttributeError) as e:
+			return e
+				
+	def _controllerFromArgs(self, opts, mout):
 		if opts.mapFile == None:
 			return Controller(mout,
-				channel=opts.channel-int(1),
-				octave=opts.octave,
-				verbose=opts.verbose)
+				channel=int(opts.channel)-1,
+				octave=int(opts.octave),
+				verbose=opts.verbose,
+				sustain=opts.sustain,
+				monophonic=opts.monophonic)
+		else:
+			keymap = self.loadKeyMap(opts.mapFile)
+			if type(keymap) == IOError:
+				print ("Error: could not find {}. Using defaults...".format(opts.mapFile))
+				opts.mapFile = None
+				return self._controllerFromArgs(opts, mout)
+			elif type(keymap) == AttributeError:
+				print ("Error: file improperly formatted. Using defaults...")
+				opts.mapFile = None
+				return self._controllerFromArgs(opts, mout)
+			else:
+				return Controller(mout,
+					pitchMap=keymap[0],
+					funcMap=keymap[1],
+					channel=int(opts.channel)-1,
+					octave=int(opts.octave),
+					verbose=opts.verbose,
+					sustain=opts.sustain,
+					monophonic=opts.monophonic)
 
 	def _getMout(self, showPorts, portName):
 		mout = rtmidi.MidiOut()
@@ -238,11 +342,11 @@ class Helper:
 			print ("Available ports:")
 			for i in range(len(ports)):
 				print ("{0}: {1}".format(i, ports[i]))
-			# keep trying until p is valid
+			# keep asking until p is valid
 			while True:
 				p = raw_input("Enter port index (leave blank to use '" + portName + "'): ")
 				try:
-					if p == '' or 0 < int(p) < len(ports): break
+					if (p == '') or (0 < int(p) < len(ports)): break
 				except:
 					pass
 
@@ -262,10 +366,6 @@ class Helper:
 def main():
 	h = Helper()
 	controller = h.createController()
-	# mout.open_virtual_port("deckydeckydecky")
-	# controller = Controller(mout, velocity=96)
-	# controller.sustain = True
-	# controller.monophonic = True
 	controller.start()
 
 main()
